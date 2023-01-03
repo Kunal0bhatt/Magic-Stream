3b86b834281ca86b111ea2280c9c6f04e7ce4d36
KunalBhattvinove <kunal.bhatt@mail.vinove.com>
2023-01-03 18:18:40 +0530

    network

---
+++ app/src/main/java/com/techapps/magicstream/network/ZoomAPI.kt
@@ -0,0 +1,40 @@
+package com.techapps.magicstream.network
+
+import com.techapps.magicstream.model.*
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+interface ZoomAPI {
+    @POST("api/login")
+    suspend fun login(
+        @Query("user_name") user_name: String,
+        @Query("password") password: String
+    ): Response<LoginResponse>
+
+    @GET("api/meetings")
+    suspend fun getMeetings(
+        @Header("Authorization") token: String
+    ): Response<MeetingResponse>
+
+    @POST("api/logout")
+    suspend fun logOut(
+        @Header("Authorization") token: String
+    ): Response<LogoutResponse>
+
+    @POST("api/change-password")
+    suspend fun changePassword(
+        @Header("Authorization") token: String,
+        @Query("oldPassword") oldPassword: String,
+        @Query("newPassword") newPassword: String
+    ): Response<ChangePasswordResponse>
+
+    @GET("api/schedule")
+    suspend fun getMatchSchedule(): Response<MatchScheduleResponse>
+
+    @GET("api/verify-token")
+    suspend fun verifyToken(
+        @Header("Authorization") token: String
+    ): Response<VerifyTokenResponse>
+}
